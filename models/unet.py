5d71930be6f2e2d2a94a1aacb4331fc6366734b5
George <giwrgosmandi@gmail.com>
2020-09-04 01:10:51 +0300

    unet experiments

--- models/unet.py
+++ models/unet.py
@@ -8,7 +8,7 @@ from keras.losses import binary_crossentropy
 import tensorflow as tf
 
 GAUSSIAN_NOISE = 0.1
-BATCH_SIZE = 8
+BATCH_SIZE = 24
 MAX_TRAIN_STEPS = 200
 
 
@@ -21,6 +21,14 @@ def dice_coef(y_true, y_pred, smooth=1):
 def dice_p_bce(in_gt, in_pred):
     return 1e-3 * binary_crossentropy(in_gt, in_pred) - dice_coef(in_gt, in_pred)
 
+## intersection over union
+def IoU(y_true, y_pred, eps=1e-6):
+    #if np.max(y_true) == 0.0:
+    if tf.math.count_nonzero(y_true) == 0:
+        return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
+    return -K.mean( (intersection + eps) / (union + eps), axis=0)
 
 class UNet:
 
@@ -78,62 +86,45 @@ class UNet:
         self.seg_model.summary()
 
     def compile(self):
-        self.seg_model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=dice_p_bce,
-                               metrics=[dice_coef, 'binary_accuracy'])
+        self.seg_model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=IoU, metrics=['binary_accuracy'])
+                               # loss=dice_p_bce, metrics=[dice_coef, 'binary_accuracy'])
 
     def validate(self, gen, input_len, valid_set, epochs=5, train_steps=MAX_TRAIN_STEPS):
-        weight_path = "serialized/unet_weights.best.hdf5".format('seg_model')
-        checkpoint = ModelCheckpoint(weight_path, monitor='val_dice_coef', verbose=1,
-                                     save_best_only=True, mode='max', save_weights_only=True)
-        reduceLROnPlat = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.5,
-                                           patience=3,
-                                           verbose=1, mode='max', epsilon=0.0001, cooldown=2, min_lr=1e-6)
-        early = EarlyStopping(monitor="val_dice_coef", mode="max", patience=50)
+        weight_path = "models/serialized/unet_weights.best.hdf5".format('seg_model')
+
+        checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=1,save_best_only=True, mode='min', save_weights_only=True)
+        reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.33,patience=3,verbose=1, mode='min', epsilon=0.0001, cooldown=0, min_lr=1e-8)
+        early = EarlyStopping(monitor="val_loss", mode="min", patience=50)
         callbacks_list = [checkpoint, early, reduceLROnPlat]
 
         valid_x = valid_set[0]
         valid_y = valid_set[1]
 
         step_count = min(train_steps, input_len // BATCH_SIZE)
-        loss_history = [self.seg_model.fit_generator(gen, steps_per_epoch=step_count, epochs=epochs,
+        history = [self.seg_model.fit_generator(gen, steps_per_epoch=step_count, epochs=epochs,
                                                      validation_data=(valid_x, valid_y),
                                                      callbacks=callbacks_list,
                                                      workers=1  # the generator is not very thread safe
                                                      )]
-        self.seg_model.load_weights(weight_path)
-        self.seg_model.save('serialized/unet.h5')
-        return loss_history
+        return history
 
     def load(self):
-        self.seg_model = tf.keras.models.load_model('serialized/unet.h5')
+        self.seg_model = self.seg_model.load_weights(weight_path)
 
     def infer(self, x):
         return self.seg_model.predict(x)
 
     def show_loss(self, loss_history):
-        epich = np.cumsum(np.concatenate(
-            [np.linspace(0.5, 1, len(mh.epoch)) for mh in loss_history]))
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(22, 13))
-        _ = ax1.plot(epich,
-                     np.concatenate([mh.history['loss'] for mh in loss_history]),
-                     'b-',
-                     epich, np.concatenate(
-                [mh.history['val_loss'] for mh in loss_history]), 'r-')
+        epochs = np.concatenate([mh.epoch for mh in loss_history])
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(22, 10))
+        
+        _ = ax1.plot(epochs, np.concatenate([mh.history['loss'] for mh in loss_history]), 'b-',
+                    epochs, np.concatenate([mh.history['val_loss'] for mh in loss_history]), 'r-')
         ax1.legend(['Training', 'Validation'])
         ax1.set_title('Loss')
-
-        _ = ax2.plot(epich, np.concatenate(
-            [mh.history['binary_accuracy'] for mh in loss_history]), 'b-',
-                     epich, np.concatenate(
-                [mh.history['val_binary_accuracy'] for mh in loss_history]),
-                     'r-')
+        
+        _ = ax2.plot(epochs, np.concatenate([mh.history['binary_accuracy'] for mh in loss_history]), 'b-',
+                    epochs, np.concatenate([mh.history['val_binary_accuracy'] for mh in loss_history]), 'r-')
         ax2.legend(['Training', 'Validation'])
         ax2.set_title('Binary Accuracy (%)')
 
-        _ = ax3.plot(epich, np.concatenate(
-            [mh.history['dice_coef'] for mh in loss_history]), 'b-',
-                     epich, np.concatenate(
-                [mh.history['val_dice_coef'] for mh in loss_history]),
-                     'r-')
-        ax3.legend(['Training', 'Validation'])
-        ax3.set_title('DICE')
