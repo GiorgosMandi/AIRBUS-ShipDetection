5d323adbd850429230966cde7156e0c88e04d1dd
George <giwrgosmandi@gmail.com>
2020-09-07 01:11:12 +0300

    model's framework

---
+++ models/segmentation_model.py
@@ -0,0 +1,69 @@
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+import keras.backend as K
+import tensorflow as tf
+from keras.optimizers import Adam
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class SegmentationModel:
+
+    def __init__(self):
+        self.seg_model = None
+        self.weights_path = None
+        self.callbacks_list = []
+
+    def set_callbacks(self):
+        checkpoint = ModelCheckpoint(self.weights_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min',
+                                     save_weights_only=True)
+        reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.33, patience=3, verbose=1, mode='min',
+                                           epsilon=0.0001, cooldown=0, min_lr=1e-8)
+        early = EarlyStopping(monitor="val_loss", mode="min", patience=50)
+        self.callbacks_list = []
+
+    def dice_coef(self, y_true, y_pred, smooth=1):
+        intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+        union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
+        return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
+
+    # intersection over union
+    def IoU(self, y_true, y_pred, eps=1e-6):
+        if tf.math.count_nonzero(y_true) == 0:
+            return self.IoU(1-y_true, 1-y_pred)
+        intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+        union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3]) - intersection
+        return -K.mean( (intersection + eps) / (union + eps), axis=0)
+
+    def compile(self):
+        self.seg_model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=self.IoU, metrics=[self.dice_coef, 'binary_accuracy'])
+
+    def load(self):
+        self.seg_model = self.seg_model.load_weights(self.weights_path)
+
+    def infer(self, x):
+        return self.seg_model.predict(x)
+
+    def validate(self, gen, input_len, valid_set, epochs=5, train_steps=200, batch_size=9):
+        valid_x = valid_set[0]
+        valid_y = valid_set[1]
+        step_count = min(train_steps, input_len // batch_size)
+        history = [self.seg_model.fit_generator(gen, steps_per_epoch=step_count, epochs=epochs,
+                                                validation_data=(valid_x, valid_y),
+                                                callbacks=self.callbacks_list,
+                                                workers=1  # the generator is not very thread safe
+                                                )]
+        return history
+
+    def show_loss(self, loss_history):
+        epochs = np.concatenate([mh.epoch for mh in loss_history])
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(22, 10))
+        
+        _ = ax1.plot(epochs, np.concatenate([mh.history['loss'] for mh in loss_history]), 'b-',
+                     epochs, np.concatenate([mh.history['val_loss'] for mh in loss_history]), 'r-')
+        ax1.legend(['Training', 'Validation'])
+        ax1.set_title('Loss')
+        
+        _ = ax2.plot(epochs, np.concatenate([mh.history['binary_accuracy'] for mh in loss_history]), 'b-',
+                     epochs, np.concatenate([mh.history['val_binary_accuracy'] for mh in loss_history]), 'r-')
+        ax2.legend(['Training', 'Validation'])
+        ax2.set_title('Binary Accuracy (%)')
--- models/unet.py
+++ models/unet.py
@@ -1,38 +1,14 @@
+from models.segmentation_model import SegmentationModel
 from keras import models, layers
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
-import numpy as np
-import matplotlib.pyplot as plt
-import keras.backend as K
-from keras.optimizers import Adam
-from keras.losses import binary_crossentropy
-import tensorflow as tf
+
 
 GAUSSIAN_NOISE = 0.1
-BATCH_SIZE = 24
-MAX_TRAIN_STEPS = 200
 
 
-def dice_coef(y_true, y_pred, smooth=1):
-    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
-    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
-    return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
-
-
-def dice_p_bce(in_gt, in_pred):
-    return 1e-3 * binary_crossentropy(in_gt, in_pred) - dice_coef(in_gt, in_pred)
-
-## intersection over union
-def IoU(y_true, y_pred, eps=1e-6):
-    #if np.max(y_true) == 0.0:
-    if tf.math.count_nonzero(y_true) == 0:
-        return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
-    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
-    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
-    return -K.mean( (intersection + eps) / (union + eps), axis=0)
-
-class UNet:
+class UNet(SegmentationModel):
 
     def __init__(self, input_len):
+        super().__init__()
         input_img = layers.Input(input_len, name='RGB_Input')
         pp_in_layer = input_img
         pp_in_layer = layers.GaussianNoise(GAUSSIAN_NOISE)(pp_in_layer)
@@ -84,47 +60,5 @@ class UNet:
 
         self.seg_model = models.Model(inputs=[input_img], outputs=[d])
         self.seg_model.summary()
-
-    def compile(self):
-        self.seg_model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=IoU, metrics=['binary_accuracy'])
-                               # loss=dice_p_bce, metrics=[dice_coef, 'binary_accuracy'])
-
-    def validate(self, gen, input_len, valid_set, epochs=5, train_steps=MAX_TRAIN_STEPS):
-        weight_path = "models/serialized/unet_weights.best.hdf5".format('seg_model')
-
-        checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=1,save_best_only=True, mode='min', save_weights_only=True)
-        reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.33,patience=3,verbose=1, mode='min', epsilon=0.0001, cooldown=0, min_lr=1e-8)
-        early = EarlyStopping(monitor="val_loss", mode="min", patience=50)
-        callbacks_list = [checkpoint, early, reduceLROnPlat]
-
-        valid_x = valid_set[0]
-        valid_y = valid_set[1]
-
-        step_count = min(train_steps, input_len // BATCH_SIZE)
-        history = [self.seg_model.fit_generator(gen, steps_per_epoch=step_count, epochs=epochs,
-                                                     validation_data=(valid_x, valid_y),
-                                                     callbacks=callbacks_list,
-                                                     workers=1  # the generator is not very thread safe
-                                                     )]
-        return history
-
-    def load(self):
-        self.seg_model = self.seg_model.load_weights(weight_path)
-
-    def infer(self, x):
-        return self.seg_model.predict(x)
-
-    def show_loss(self, loss_history):
-        epochs = np.concatenate([mh.epoch for mh in loss_history])
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(22, 10))
-        
-        _ = ax1.plot(epochs, np.concatenate([mh.history['loss'] for mh in loss_history]), 'b-',
-                    epochs, np.concatenate([mh.history['val_loss'] for mh in loss_history]), 'r-')
-        ax1.legend(['Training', 'Validation'])
-        ax1.set_title('Loss')
-        
-        _ = ax2.plot(epochs, np.concatenate([mh.history['binary_accuracy'] for mh in loss_history]), 'b-',
-                    epochs, np.concatenate([mh.history['val_binary_accuracy'] for mh in loss_history]), 'r-')
-        ax2.legend(['Training', 'Validation'])
-        ax2.set_title('Binary Accuracy (%)')
-
+        self.weight_path = "models/serialized/unet_weights.best.hdf5".format('seg_model')
+        self.set_callbacks()
